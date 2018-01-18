530efe5933c1859d10c934b5bd53d82597114989
David Bohan <madvlad00@googlemail.com>
2018-01-17 22:50:56 -0500

    Add 4D vector operations

--- core/src/math/Vector4D.ts
+++ core/src/math/Vector4D.ts
@@ -7,4 +7,22 @@ export class Vector4D {
     constructor(x: number, y: number, z: number, w: number) {
         this.x = x; this.y = y; this.z = z; this.w = w;
     }
+
+    scalarMultiply(s: number): Vector4D {
+        this.multiplyMembers(s);
+        return this;
+    }
+
+    scalarDivide(s: number): Vector4D {
+        s = 1 / s;
+        this.multiplyMembers(s);
+        return this;
+    }
+
+    private multiplyMembers(s: number) {
+        this.x *= s;
+        this.y *= s;
+        this.z *= s;
+        this.w *= s;
+    }
 }
